aa0529e0d9b4b60749024686313075b1f86503d2
김현희 <tta_bi@naver.com>
2022-01-06 10:00:28 +0900

    레트로핏 객체 생성 준비중 - 어느 서버에 연결할 것인지? 주소 입력 - 객체를 담게 될 변수 : 직접 접근 불허 (private) - 객체를 받아올 때 사용할 함수 : getRetrofit

--- app/src/main/java/com/nepplus/apipractice_20220106/api/ServerAPI.kt
+++ app/src/main/java/com/nepplus/apipractice_20220106/api/ServerAPI.kt
@@ -1,5 +1,7 @@
 package com.nepplus.apipractice_20220106.api
 
+import retrofit2.Retrofit
+
 class ServerAPI {
 
 //    서버 통신을 하게 도와주는 클래스 (Retrofit)를 객체로 만들어주는 기능
@@ -9,6 +11,17 @@ class ServerAPI {
 //    클래스 이름.기능() => @classmethod 에 대응되는 기능 활용
 
     companion object{
+
+//    이 { } 안에 적는 변수 / 함수들은 => ServerAPI.변수/함수로 활용 가능
+
+        private val BASE_URL = "https://api.gudoc.in"   // 기본 보안 처리는 되어있는 서버 http"s" => 이 클래스 내부에서만 사용예정
+        private var retrofit : Retrofit? = null  // 서버 연결 전담 변수 => 기본적으로는 아직 만들지 않은 상태(null)
+
+//        retrofit 변수에 환경 설정 + 객체화 => 가져다 쓸 수 있게 하는 기능 (함수)
+
+        fun getRetrofit() : Retrofit {
+
+        }
 
 
     }
