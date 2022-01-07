83c1e4f0fe154a850085a4d0a2d08605d37b1a02
김현희 <tta_bi@naver.com>
2022-01-07 15:20:23 +0900

    리뷰 상세보기 데이터 반영 마무리

--- app/src/main/java/com/nepplus/apipractice_20220106/ViewReviewActivity.kt
+++ app/src/main/java/com/nepplus/apipractice_20220106/ViewReviewActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.apipractice_20220106
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.nepplus.apipractice_20220106.models.BasicResponse
 import com.nepplus.apipractice_20220106.models.ReviewData
 import kotlinx.android.synthetic.main.activity_view_review.*
@@ -26,6 +27,13 @@ class ViewReviewActivity : BaseActivity() {
 
                 txtReviewTitle.text = newReviewData.title
                 txtReviewContent.text = newReviewData.content
+
+                Glide.with(mContext).load(newReviewData.thumbnail_img).into(imgReciewThumbnail)
+                Glide.with(mContext).load(newReviewData.user.profile_img).into(imgWriterProfile)
+
+                txtWriterNickname.text = newReviewData.user.nick_name
+
+                scoreRatingBar.rating = newReviewData.score
 
             }
 
--- app/src/main/java/com/nepplus/apipractice_20220106/models/ReviewData.kt
+++ app/src/main/java/com/nepplus/apipractice_20220106/models/ReviewData.kt
@@ -6,7 +6,7 @@ class ReviewData(
     val id : Int,
     val title : String,
     val content : String,
-    val score : Double,  // 파이썬에서는 float개념과 동일
+    val score : Float,  // 파이썬에서는 float개념과 동일
     val thumbnail_img : String,
     val user : UserData  // 기존에 만든 UserData 재활용
 ) :Serializable {
