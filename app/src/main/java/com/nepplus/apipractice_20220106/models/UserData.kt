6c132c00a26d12e9f74cd9f8a8b3c36fabc58069
김현희 <tta_bi@naver.com>
2022-01-07 10:14:31 +0900

    이메일 / 폰번 추가 파싱, UI 활용

--- app/src/main/java/com/nepplus/apipractice_20220106/models/UserData.kt
+++ app/src/main/java/com/nepplus/apipractice_20220106/models/UserData.kt
@@ -3,6 +3,8 @@ package com.nepplus.apipractice_20220106.models
 class UserData(
     val id : Int,
     val nick_name : String,
-    val profile_img : String
+    val profile_img : String,
+    val email : String,
+    val phone : String
 ) {
 }
