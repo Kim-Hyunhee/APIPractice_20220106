4a9ace760587ffedde17ed6e7d617f5df1d3e58e
김현희 <tta_bi@naver.com>
2022-01-07 12:02:47 +0900

    리뷰 등록 이벤트 처리중

--- app/src/main/java/com/nepplus/apipractice_20220106/EditReviewActivity.kt
+++ app/src/main/java/com/nepplus/apipractice_20220106/EditReviewActivity.kt
@@ -2,10 +2,26 @@ package com.nepplus.apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.apipractice_20220106.models.ProductData
+import com.neppplus.apipractice_20220106.BaseActivity
+import kotlinx.android.synthetic.main.activity_edit_review.*
 
-class EditReviewActivity : AppCompatActivity() {
+class EditReviewActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_review)
+
+//        이전 화면이 넘겨준 상품 데이터 받아오기
+        val product = intent.getSerializableExtra("product") as ProductData
+
+        btnSubmitReview.setOnClickListener {
+
+            val inputTitle = edtTitle.toString()
+            val inputContent = edtContent.toString()
+            val inputScore = ratingBar.rating
+
+//            서버에 전달하자 -> POST / review
+
+        }
     }
 }
